a16c02e0a017782819d3893cb41ddf092e034854
CodrinCristea-si <codrincristea210@gmail.com>
2023-05-23 05:01:11 +0300

    bug fix, the LuCI interface does not display the infectivity type properly

--- luci_interface/infectivity_view/files/script.js
+++ luci_interface/infectivity_view/files/script.js
@@ -8,51 +8,49 @@ var correspondence=[
     [5,"Infected","Sever"]
 ];
 
-function create_primary_select(id){
+function create_select(values,id, default_val){
+	let select=document.createElement("select");
+    select.className="select";
+    select.id=id;
+    for (let infec of values){
+        let opt= document.createElement("option");
+        opt.className="option";
+        opt.value=infec;
+		if (infec == default_val)
+			opt.selected = true;
+        opt.text=infec;
+        select.appendChild(opt);
+    }
+	return select;
+}
+
+function create_primary_select(id, default_val){
     let infectivity =[];
     for (let cor of correspondence){
         if (!(infectivity.find(inf => inf == cor[1])))
             infectivity.push(cor[1]);
     }
-    let select=document.createElement("select");
-    select.className="select";
-    select.id=id;
-    for (let infec of infectivity){
-        let opt= document.createElement("option");
-        opt.className="option";
-        opt.value=infec;
-        opt.text=infec;
-        select.appendChild(opt);
-    }
-    return select;
+	//console.log("prim" + infectivity);
+    return create_select(infectivity, id, default_val);
 }
 
-function create_secondary_select(primary_infec, id){
+function create_secondary_select(primary_infec, id, default_val){
     let infectivity =[];
     //let primary_infec = correspondence.find( cor => cor[0] == type_infec);
     for (let cor of correspondence){
         if (primary_infec == cor[1] && !(infectivity.find(inf => inf == cor[2])))
             infectivity.push(cor[2]);
     }
-    let select=document.createElement("select");
-    select.className="select";
-    select.id=id;
-    for (let infec of infectivity){
-        let opt= document.createElement("option");
-        opt.className="option";
-        opt.value=infec;
-        opt.text=infec;
-        select.appendChild(opt);
-    }
-    return select;
+    //console.log("second" + infectivity);
+    return create_select(infectivity,id,default_val);
 }
 
 function update_clients(list_cl){
-    console.log(list_cl);
+    //console.log(list_cl);
     let list_clients = document.getElementById("list_clients");
     let line_nr = 0;
     for(let client of list_cl){
-        console.log(client);
+        //console.log(client);
         line_nr++;
         let cl_elm = document.createElement("tr");
         cl_elm.className="tr cbi-rowstyle-1";
@@ -75,43 +73,53 @@ function update_clients(list_cl){
             let infec_pr=cor[1];
             let infec_sec=cor[2];
 
-            console.log(type_infec, infec_pr, infec_sec);
+            // console.log(type_infec, infec_pr, infec_sec);
 
+            //for infectivity types
             let attr_elem3 = document.createElement("td");
             attr_elem3.className="td";
-
-            let select1 = create_primary_select("primary-"+line_nr);
+            //div for selects
+			let div3 = document.createElement("div");
+            //div3.className="select-container"
+            //select infectivity primary
+            let select1 = create_primary_select("primary-"+line_nr, infec_pr);
             
-            select1.value=infec_pr;
-            //select1.text=infec_pr
-            //rework
             select1.addEventListener("change",((event)=>{
                 console.log("event");
-                let id_sel = event.target.id.split('-')[1];
-                let select =document.getElementById("secondary" + id_sel);
-                let parent = select.parentNode();
-                let select_new = create_secondary_select(value, select.id);
-
-                parent.removeChild(select);
+                let id_sel = "secondary-" + event.target.id.split('-')[1];
+				console.log(id_sel);
+                let select =document.getElementById(id_sel);
+				console.log(select);
+                let parent = select.parentElement;
+				parent.removeChild(select);
+                let select_new = create_secondary_select(event.target.value, id_sel,"");
                 parent.appendChild(select_new);
             }));
-            console.log(select1.value, infec_pr);
-            console.log(select1);
-            attr_elem3.appendChild(select1);
-            select1.onchange=function(){console.log("salut")}
+            // console.log(select1.value, infec_pr);
+            // console.log(select1);
+            div3.appendChild(select1);
+            //select1.onchange=function(){console.log("salut")}
+            
+            //this cause mozzilla to f**k the first select, idk why, ask W3WC
+            //div3.innerHTML += "<br>";
+            //attr_elem3.style.display = "flex";
+            //div3.style.display = "flex";
 
-            attr_elem3.innerHTML += "<br><br>";
-
-            let select2 = create_secondary_select(infec_pr,"secondary-"+line_nr);
+            //select for infectivity secondary
+            let select2 = create_secondary_select(infec_pr,"secondary-"+line_nr, infec_sec);
             select2.value=infec_sec;
-            attr_elem3.appendChild(select2);
-            select2.onchange=function(){console.log("salut2")}
+            div3.appendChild(select2);
+            select2.onchange=function(){console.log("salut2")};
+			attr_elem3.appendChild(div3);
 
             cl_elm.appendChild(attr_elem3);
             //for actions
             let attr_elem4 = document.createElement("td");
             attr_elem4.className="td";
             let but = document.createElement("button")
+			but.addEventListener("click",function(){
+				console.log("click");
+			})
             but.className="cbi-button cbi-button-edit";
             but.innerHTML="Save";
             attr_elem4.appendChild(but);
@@ -126,10 +134,10 @@ function load_data(){
     L.Request.get(window.location.href+'/load',null).then(
         function(data){
             var list_clients=[];
-            console.log(data);
+            //console.log(data);
             let payload = data.json()
             let ls_cl= payload["data"];
-            console.log(ls_cl);
+            //console.log(ls_cl);
             for (let el of ls_cl){
                 let client = {ip:"", mac:"",infect:""}
                 client.ip=el.ip;
@@ -147,7 +155,9 @@ function execute_start(){
     // let cl3 = {ip:"192.168.1.100", mac:"45:45:45:45:45:45",infect:"3"};
     // let cl4 = {ip:"192.168.1.56", mac:"12:12:12:12:12:12",infect:"4"};
     // let cl5 = {ip:"192.168.1.76", mac:"cd:cd:dc:dc:cd:cd",infect:"5"};
+	// let list_cl =[cl1,cl2,cl3,cl4,cl5];
     load_data();
+	//update_clients(list_cl);
     
 }
 
@@ -155,68 +165,3 @@ console.log("start");
 window.onload=(()=>{
     execute_start();
 });
-
-
-// <!-- <script src="/luci-static/resources/luci.js"></script> -->
-// <!-- <script type="text/javascript" src="<%=luci.dispatcher.build_url('network/infectivity/home-script.js')%>"> </script> -->
-// <!-- <script type="text/javascript" src="/view/network/infectivity/home-script.js"></script> -->
-// <!-- <%=luci.util.lib("luci-static.resources.my_script")%> -->
-
-// <!-- <div class="network-status-table">
-//     <div class="ifacebox">
-//         <div class="ifacebox-head center active">
-//             <strong>radio0</strong>
-//         </div>
-//         <div class="ifacebox-body left">
-//             <span>
-//                 <span class="nowrap">
-//                     <strong>Type: 
-
-//                     </strong>MediaTek MT76x8 802.11bgn</span><br>
-//                     <span class="nowrap">
-//                         <strong>Channel: </strong>
-//                         13 (2.472 GHz)
-//                     </span><br>
-//                     <span class="nowrap"><strong>Bitrate: </strong>54 Mbit/s</span></span><div><span class="ifacebadge"><img src="/luci-static/resources/icons/signal-75-100.png" title="Signal: -24 dBm / Quality: 100%"><span><span class="nowrap"><strong>SSID: </strong>OpenWrt</span><br><span class="nowrap"><strong>Mode: </strong>Master</span><br><span class="nowrap"><strong>BSSID: </strong>32:DE:4B:5A:BB:CC</span><br><span class="nowrap"><strong>Encryption: </strong>WPA2 PSK (CCMP)</span><br><span class="nowrap"><strong>Associations: </strong>1</span><br></span></span><span class="ifacebadge"><img src="/luci-static/resources/icons/signal-75-100.png" title="Signal: -25 dBm / Quality: 100%"><span><span class="nowrap"><strong>SSID: </strong>Parola este 35789473</span><br><span class="nowrap"><strong>Mode: </strong>Client</span><br><span class="nowrap"><strong>BSSID: </strong>30:DE:4B:5A:BB:CC</span><br><span class="nowrap"><strong>Encryption: </strong>WPA2 PSK (CCMP)</span><br><span class="nowrap"><strong>Associations: </strong>1</span><br></span></span></div></div></div></div>
-//                     <h3>Associated Stations</h3>
-//                     <table class="table assoclist" data-idref="73bd1ea2">
-//                         <tr class="tr table-titles">
-//                             <th class="th nowrap" data-sortable-row="true">Network</th>
-//                             <th class="th hide-xs" data-sortable-row="true">MAC address</th>
-//                             <th class="th" data-sortable-row="true">Host</th>
-//                             <th class="th" data-sortable-row="true">Signal / Noise</th>
-//                             <th class="th" data-sortable-row="true">RX Rate / TX Rate</th>
-//                             <th class="th cbi-section-actions"></th>
-//                         </tr>
-//                         <tr class="tr cbi-rowstyle-1">
-//                             <td class="td nowrap" data-title="Network">
-//                                 <span class="ifacebadge" title="Wireless Network: Master &quot;OpenWrt&quot; (wlan0-1)" data-ifname="wlan0-1" data-ssid="OpenWrt">
-//                                     <img src="/luci-static/resources/icons/wifi.png">
-//                                     <span> Master "OpenWrt"<small> (wlan0-1)</small></span>
-//                                 </span>
-//                             </td>
-//                             <td class="td hide-xs" data-title="MAC address">BC:D0:74:84:95:78</td>
-//                             <td class="td" data-title="Host">Cristeas-MBP.lan <span class="hide-xs">(192.168.1.123, fe80::c1b:698c:2c56:9dd)</span></td>
-//                             <td class="td" data-title="Signal / Noise">
-//                                 <span class="ifacebadge" title="Signal: -24 dBm" data-signal="-24" data-noise="0">
-//                                     <img src="/luci-static/resources/icons/signal-75-100.png">
-//                                     <span> -24&nbsp;dBm</span>
-//                                 </span>
-//                             </td>
-//                             <td class="td" data-title="RX Rate / TX Rate">
-//                                 <span>
-//                                     <span>54.0&nbsp;Mbit/s, 20&nbsp;MHz</span><br>
-//                                     <span>54.0&nbsp;Mbit/s, 20&nbsp;MHz</span>
-//                                 </span>
-//                             </td>
-//                             <td class="td cbi-section-actions">
-//                                 <button class="cbi-button cbi-button-remove">Disconnect</button>
-//                             </td>
-//                         </tr>
-
-
-//                         <tr class="tr cbi-rowstyle-2"><td class="td nowrap" data-title="Network"><span class="ifacebadge" title="Wireless Network: Client &quot;Parola este 35789473&quot; (wlan0)" data-ifname="wlan0" data-ssid="Parola este 35789473"><img src="/luci-static/resources/icons/wifi.png"><span> Client "Parola este 35789473"<small> (wlan0)</small></span></span></td><td class="td hide-xs" data-title="MAC address">DC:9B:D6:B5:62:1C</td><td class="td" data-title="Host">?</td><td class="td" data-title="Signal / Noise"><span class="ifacebadge" title="Signal: -26 dBm" data-signal="-26" data-noise="0"><img src="/luci-static/resources/icons/signal-75-100.png"><span> -26&nbsp;dBm</span></span></td><td class="td" data-title="RX Rate / TX Rate"><span><span>144.4&nbsp;Mbit/s, 20&nbsp;MHz, MCS&nbsp;15, Short&nbsp;GI</span><br><span>144.4&nbsp;Mbit/s, 20&nbsp;MHz, MCS&nbsp;15, Short&nbsp;GI</span></span></td><td class="td cbi-section-actions">-</td></tr></table>
-
-
-
-//                     <div class="network-status-table"><div class="ifacebox"><div class="ifacebox-head center active"><strong>radio0</strong></div><div class="ifacebox-body left"><span><span class="nowrap"><strong>Type: </strong>MediaTek MT76x8 802.11bgn</span><br><span class="nowrap"><strong>Channel: </strong>13 (2.472 GHz)</span><br><span class="nowrap"><strong>Bitrate: </strong>54 Mbit/s</span></span><div><span class="ifacebadge"><img src="/luci-static/resources/icons/signal-75-100.png" title="Signal: -25 dBm / Quality: 100%"><span><span class="nowrap"><strong>SSID: </strong>OpenWrt</span><br><span class="nowrap"><strong>Mode: </strong>Master</span><br><span class="nowrap"><strong>BSSID: </strong>32:DE:4B:5A:BB:CC</span><br><span class="nowrap"><strong>Encryption: </strong>WPA2 PSK (CCMP)</span><br><span class="nowrap"><strong>Associations: </strong>1</span><br></span></span><span class="ifacebadge"><img src="/luci-static/resources/icons/signal-75-100.png" title="Signal: -26 dBm / Quality: 100%"><span><span class="nowrap"><strong>SSID: </strong>Parola este 35789473</span><br><span class="nowrap"><strong>Mode: </strong>Client</span><br><span class="nowrap"><strong>BSSID: </strong>30:DE:4B:5A:BB:CC</span><br><span class="nowrap"><strong>Encryption: </strong>WPA2 PSK (CCMP)</span><br><span class="nowrap"><strong>Associations: </strong>1</span><br></span></span></div></div></div></div><h3>Associated Stations</h3><table class="table assoclist" data-idref="5dcb517d"><tr class="tr table-titles"><th class="th nowrap" data-sortable-row="true">Network</th><th class="th hide-xs" data-sortable-row="true">MAC address</th><th class="th" data-sortable-row="true">Host</th><th class="th" data-sortable-row="true">Signal / Noise</th><th class="th" data-sortable-row="true">RX Rate / TX Rate</th><th class="th cbi-section-actions"></th></tr><tr class="tr cbi-rowstyle-1"><td class="td nowrap" data-title="Network"><span class="ifacebadge" title="Wireless Network: Master &quot;OpenWrt&quot; (wlan0-1)" data-ifname="wlan0-1" data-ssid="OpenWrt"><img src="/luci-static/resources/icons/wifi.png"><span> Master "OpenWrt"<small> (wlan0-1)</small></span></span></td><td class="td hide-xs" data-title="MAC address">BC:D0:74:84:95:78</td><td class="td" data-title="Host">Cristeas-MBP.lan <span class="hide-xs">(192.168.1.123, fe80::c1b:698c:2c56:9dd)</span></td><td class="td" data-title="Signal / Noise"><span class="ifacebadge" title="Signal: -24 dBm" data-signal="-24" data-noise="0"><img src="/luci-static/resources/icons/signal-75-100.png"><span> -24&nbsp;dBm</span></span></td><td class="td" data-title="RX Rate / TX Rate"><span><span>54.0&nbsp;Mbit/s, 20&nbsp;MHz</span><br><span>54.0&nbsp;Mbit/s, 20&nbsp;MHz</span></span></td><td class="td cbi-section-actions"><button class="cbi-button cbi-button-remove">Disconnect</button></td></tr><tr class="tr cbi-rowstyle-2"><td class="td nowrap" data-title="Network"><span class="ifacebadge" title="Wireless Network: Client &quot;Parola este 35789473&quot; (wlan0)" data-ifname="wlan0" data-ssid="Parola este 35789473"><img src="/luci-static/resources/icons/wifi.png"><span> Client "Parola este 35789473"<small> (wlan0)</small></span></span></td><td class="td hide-xs" data-title="MAC address">DC:9B:D6:B5:62:1C</td><td class="td" data-title="Host">?</td><td class="td" data-title="Signal / Noise"><span class="ifacebadge" title="Signal: -28 dBm" data-signal="-28" data-noise="0"><img src="/luci-static/resources/icons/signal-75-100.png"><span> -28&nbsp;dBm</span></span></td><td class="td" data-title="RX Rate / TX Rate"><span><span>144.4&nbsp;Mbit/s, 20&nbsp;MHz, MCS&nbsp;15, Short&nbsp;GI</span><br><span>144.4&nbsp;Mbit/s, 20&nbsp;MHz, MCS&nbsp;15, Short&nbsp;GI</span></span></td><td class="td cbi-section-actions">-</td></tr></table> -->
