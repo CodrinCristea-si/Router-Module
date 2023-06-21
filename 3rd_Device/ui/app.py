ef598bf1eb991824529ce5414f0b1555e085baf6
CodrinCristea-si <codrincristea210@gmail.com>
2023-06-21 11:31:27 +0300

    Add UI clients tab

--- 3rd_Device/ui/app.py
+++ 3rd_Device/ui/app.py
@@ -17,3 +17,20 @@ def index():
     #url_for('static', filename='css/main-page.css')
     return render_template('index.html')
 
+@app.route('/clients')
+def clients():
+    # lista = [ Client(ClientID=1, CurrentIP= "192.168.1.4", MAC="00:11:22:33:44:55", IsConnected = 1, Score=20, IsTesting=0, InfectivityType=InfectivityTypes.DEFAULT.value),
+    #           Client(ClientID=2, CurrentIP="192.168.1.6", MAC="34:45:78:ed:aa:bf", IsConnected=1, Score=200, IsTesting=0, InfectivityType=InfectivityTypes.INFECTED_MAJOR.value),
+    #           Client(ClientID=3, CurrentIP="192.168.1.7", MAC="aa:bb:cc:dd:ee:ff", IsConnected=1, Score=100,
+    #                  IsTesting=1, InfectivityType=InfectivityTypes.INFECTED_MINOR.value),
+    #           Client(ClientID=4, CurrentIP="192.168.1.10", MAC="11:ff:44:aa:dd:55", IsConnected=0, Score=0,
+    #                  IsTesting=0, InfectivityType=InfectivityTypes.UNINFECTED.value)
+    #           ]
+    comm = ITC("127.0.0.1", 5004, Logger())
+    comm.connect()
+    pack = InfectivityRequest(InfectivityRequestType.GET_ALL_CLIENTS, [])
+    comm.send_request(pack)
+    resp = comm.read_response()
+    lista = resp.payload
+    return render_template("clients.html", lista = lista)
+
